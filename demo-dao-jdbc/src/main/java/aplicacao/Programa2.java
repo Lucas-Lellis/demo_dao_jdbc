143fab09ccd33d085de08c584282750c1e43f611
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-06-19 17:34:41 -0300

    implementacao DEP acharTodos

---
+++ demo-dao-jdbc/src/main/java/aplicacao/Programa2.java
@@ -0,0 +1,23 @@
+package aplicacao;
+
+import model.dao.DepartamentoDao;
+import model.dao.FabricaDao;
+import model.entidades.Departamento;
+import model.entidades.Vendedor;
+
+import java.util.List;
+
+public class Programa2 {
+
+    public static void main(String[] args) {
+
+        DepartamentoDao departamentoDao = FabricaDao.criarDepartamentoDao();
+
+        System.out.println("=== TESTE 1: Departamento AcharTodos ===");
+        List<Departamento> list = departamentoDao.acharTodos();
+        for (Departamento dep : list) {
+            System.out.println(dep);
+        }
+
+    }
+}
---
+++ demo-dao-jdbc/src/main/java/model/dao/impl/DepartamentoDaoJDBC.java
@@ -0,0 +1,78 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartamentoDao;
+import model.entidades.Departamento;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartamentoDaoJDBC implements DepartamentoDao {
+
+    private Connection conn;
+
+    public DepartamentoDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void inserir(Departamento obj) {
+
+    }
+
+    @Override
+    public void atualizar(Departamento obj) {
+
+    }
+
+    @Override
+    public void apagar(Integer id) {
+
+    }
+
+    @Override
+    public Departamento acharPorId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Departamento> acharTodos() {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * FROM department " +
+                        "ORDER BY Id "
+            );
+
+            rs = st.executeQuery();
+
+            List<Departamento> list = new ArrayList<>();
+
+            while (rs.next()) {
+                Departamento dep = instanciarDepartamento(rs);
+                list.add(dep);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private Departamento instanciarDepartamento(ResultSet rs) throws SQLException {
+        Departamento dep = new Departamento();
+        dep.setId(rs.getInt("Id"));
+        dep.setNome(rs.getString("Name"));
+        return dep;
+    }
+}
